fb1e965cedb9948b4aef0da76b3cf1973ebe7991
St.Onge <stonge.ms@gmail.com>
2021-06-23 20:47:40 -0700

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 $(function() {
+    
+    
+function setTime() {
+        // Sets interval in variable
+        var timerInterval = setInterval(function() {
+          secondsLeft--;
+          timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+      
+          if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            sendMessage();
+          }
+      
+        }, 1000);
+      }
+        
+        `assets\img\stongeIcon (${#}).png`
+
 
     $('.navbar-toggle').click(function() {
         $(this).toggleClass('act');
@@ -87,3 +107,4 @@ $(function() {
     });
     
 });
+
